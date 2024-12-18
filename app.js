4d5d21ea8ffc262363f6793fde883123bbe616a4
choimunkil <mg.choi@megaworks.ai>
2024-12-18 20:39:35 +0900

    init2

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const app = express();
 const http = require("http");
-const server = http.createServer(app);
 const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: true,
   },
 });
+
 // chat namespace 사용
 const chat = io.of("/chat");
 
@@ -18,18 +19,17 @@ chat.on("connection", (socket) => {
   const roomIdx = socket.handshake.query.roomIdx;
   socket.join(roomIdx);
   socket.to(roomIdx).emit("join", roomIdx + " chatroom join");
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
     socket.leave(roomIdx);
   });
 
-  // on으로 이벤트 받고 emit으로 보내줌
   socket.on("send", (msg) => {
     socket.emit("receive", msg);
     socket.to(roomIdx).emit("receive", msg);
   });
 });
 
-server.listen(3010, () => {
-  console.log("server listen 3010 port...");
-});
+// Vercel은 module.exports를 통해 앱을 내보냄
+module.exports = app;
